679e1a15253090eb77204de647873fccf21689a4
Marina <marinacorreas56@gmail.com>
2025-07-21 20:41:29 -0300

    Salvando todas as minhas mudanças antes do pull

--- database/migrations/2025_07_21_224808_add_is_admin_to_users_table.php
+++ database/migrations/2025_07_21_224808_add_is_admin_to_users_table.php
@@ -4,19 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIsAdminToUsersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_admin')->default(false)->after('email');
+            //
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('is_admin');
+            //
         });
     }
-}
+};
